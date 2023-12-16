a2ffb6f2d01b95415a9095799d1edeb2498bb910
mediumexe <149566514+mediumexe@users.noreply.github.com>
2023-12-16 23:07:27 +0300

    Add files via upload

---
+++ 2 кр.pas
@@ -0,0 +1,21 @@
+﻿var
+  arr: array of Integer;
+  n, i, sum: Integer;
+begin
+  Write('Введите размер массива: ');
+  Readln(n);
+  SetLength(arr, n);
+  WriteLn('Введите элементы массива:');
+  for i := 0 to n - 1 do
+  begin
+    Write('arr[', i, ']: ');
+    Readln(arr[i]);
+  end;
+  sum := 0;
+  for i := 0 to n - 1 do
+  begin
+    if i mod 2 = 0 then
+      sum := sum + arr[i];
+  end;
+  WriteLn('Сумма элементов на четных позициях: ', sum);
+end.
---
+++ 2 крю.pas
@@ -0,0 +1,24 @@
+﻿var
+  str: string;
+  wordCount, letterCount, i: Integer;
+begin
+  WriteLn('Введите строку:');
+  ReadLn(str);
+  wordCount := 0;
+  letterCount := 0;
+  while (Length(str) > 0) and (str[1] = ' ') do
+    Delete(str, 1, 1);
+  while (Length(str) > 0) and (str[Length(str)] = ' ') do
+    Delete(str, Length(str), 1);
+  for i := 1 to Length(str) do
+  begin
+    if (str[i] <> ' ') and (str[i] <> ',') and (str[i] <> '.') then
+      letterCount := letterCount + 1;
+    if (i < Length(str)) and ((str[i] = ' ') or (str[i] = ',') or (str[i] = '.')) and (str[i+1] <> ' ') then
+      wordCount := wordCount + 1;
+  end;
+  if Length(str) > 0 then
+    wordCount := wordCount + 1;
+  WriteLn('Число слов в строке: ', wordCount);
+  WriteLn('Число букв в строке: ', letterCount);
+end.
